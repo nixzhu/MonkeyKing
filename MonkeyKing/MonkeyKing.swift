9ae4a652c1313df1838e186d0c9106d4a160f8f7
nixzhu <zhuhongxu@gmail.com>
2015-11-13 15:11:38 +0800

    fix monkeyking_QQCallbackName

--- MonkeyKing/MonkeyKing.swift
+++ MonkeyKing/MonkeyKing.swift
@@ -947,12 +947,12 @@ private extension String {
 
     var monkeyking_QQCallbackName: String {
 
-        var callbackName = String(format: "QQ%02llx", (self as NSString).longLongValue)
-        while callbackName.characters.count < 0 {
-            callbackName = "0" + callbackName
+        var hexString = String(format: "%02llx", (self as NSString).longLongValue)
+        while hexString.characters.count < 0 {
+            hexString = "0" + hexString
         }
 
-        return callbackName
+        return "QQ" + hexString
     }
 }
 
