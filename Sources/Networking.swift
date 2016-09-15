3c93cb6de57d55ea4b734bd1147bb7bd1465e16b
TBXark <vfanx@qq.com>
2016-09-15 16:55:21 +0800

    update to swift3.0

--- Sources/Networking.swift
+++ Sources/Networking.swift
@@ -11,9 +11,9 @@ import Foundation
 class Networking {
 
     static let sharedInstance = Networking()
-    private let session = NSURLSession.sharedSession()
+    fileprivate let session = URLSession.shared
 
-    typealias NetworkingResponseHandler = (NSDictionary?, NSURLResponse?, NSError?) -> Void
+    typealias NetworkingResponseHandler = (NSDictionary?, URLResponse?, NSError?) -> Void
     
     enum Method: String {
         case GET
@@ -21,31 +21,31 @@ class Networking {
     }
 
     enum ParameterEncoding {
-        case URL
-        case URLEncodedInURL
-        case JSON
+        case url
+        case urlEncodedInURL
+        case json
 
-        func encode(URLRequest: NSMutableURLRequest, parameters: [String: AnyObject]?) -> NSURLRequest {
-            guard let parameters = parameters, mutableURLRequest = URLRequest.mutableCopy() as? NSMutableURLRequest  else {
-                return URLRequest
+        func encode(_ URLRequest: NSMutableURLRequest, parameters: [String: AnyObject]?) -> Foundation.URLRequest {
+            guard let parameters = parameters, let mutableURLRequest = URLRequest.mutableCopy() as? NSMutableURLRequest  else {
+                return URLRequest as URLRequest
             }
 
             switch self {
-            case .URL, .URLEncodedInURL:
-                func query(parameters: [String: AnyObject]) -> String {
+            case .url, .urlEncodedInURL:
+                func query(_ parameters: [String: AnyObject]) -> String {
                     var components: [(String, String)] = []
 
-                    for key in parameters.keys.sort(<) {
+                    for key in parameters.keys.sorted(by: <) {
                         let value = parameters[key]!
                         components += queryComponents(key, value)
                     }
 
-                    return (components.map { "\($0)=\($1)" } as [String]).joinWithSeparator("&")
+                    return (components.map { "\($0)=\($1)" } as [String]).joined(separator: "&")
                 }
 
-                func encodesParametersInURL(method: Method) -> Bool {
+                func encodesParametersInURL(_ method: Method) -> Bool {
                     switch self {
-                    case .URLEncodedInURL:
+                    case .urlEncodedInURL:
                         return true
                     default:
                         break
@@ -59,42 +59,42 @@ class Networking {
                     }
                 }
 
-                if let method = Method(rawValue: mutableURLRequest.HTTPMethod) where encodesParametersInURL(method) {
-                    if let URLComponents = NSURLComponents(URL: mutableURLRequest.URL!, resolvingAgainstBaseURL: false) {
+                if let method = Method(rawValue: mutableURLRequest.httpMethod) , encodesParametersInURL(method) {
+                    if let URLComponents = URLComponents(url: mutableURLRequest.url!, resolvingAgainstBaseURL: false) {
                         let percentEncodedQuery = (URLComponents.percentEncodedQuery.map { $0 + "&" } ?? "") + query(parameters)
                         URLComponents.percentEncodedQuery = percentEncodedQuery
-                        mutableURLRequest.URL = URLComponents.URL
+                        mutableURLRequest.url = URLComponents.url
                     }
                 } else {
-                    if mutableURLRequest.valueForHTTPHeaderField("Content-Type") == nil {
+                    if mutableURLRequest.value(forHTTPHeaderField: "Content-Type") == nil {
                         mutableURLRequest.setValue(
                             "application/x-www-form-urlencoded; charset=utf-8",
                             forHTTPHeaderField: "Content-Type"
                         )
                     }
 
-                    mutableURLRequest.HTTPBody = query(parameters).dataUsingEncoding(
-                        NSUTF8StringEncoding,
+                    mutableURLRequest.httpBody = query(parameters).data(
+                        using: String.Encoding.utf8,
                         allowLossyConversion: false
                     )
                 }
-            case .JSON:
+            case .json:
                 do {
-                    let options = NSJSONWritingOptions()
-                    let data = try NSJSONSerialization.dataWithJSONObject(parameters, options: options)
+                    let options = JSONSerialization.WritingOptions()
+                    let data = try JSONSerialization.data(withJSONObject: parameters, options: options)
 
                     mutableURLRequest.setValue("application/json; charset=UTF-8", forHTTPHeaderField: "Content-Type")
                     mutableURLRequest.setValue("application/json", forHTTPHeaderField: "X-Accept")
-                    mutableURLRequest.HTTPBody = data
+                    mutableURLRequest.httpBody = data
                 } catch {
                 }
 
             }
 
-            return mutableURLRequest
+            return mutableURLRequest as URLRequest
         }
 
-        func queryComponents(key: String, _ value: AnyObject) -> [(String, String)] {
+        func queryComponents(_ key: String, _ value: AnyObject) -> [(String, String)] {
             var components: [(String, String)] = []
 
             if let dictionary = value as? [String: AnyObject] {
@@ -112,17 +112,17 @@ class Networking {
             return components
         }
 
-        func escape(string: String) -> String {
+        func escape(_ string: String) -> String {
             let generalDelimitersToEncode = ":#[]@" // does not include "?" or "/" due to RFC 3986 - Section 3.4
             let subDelimitersToEncode = "!$&'()*+,;="
 
-            let allowedCharacterSet = NSCharacterSet.URLQueryAllowedCharacterSet().mutableCopy() as! NSMutableCharacterSet
-            allowedCharacterSet.removeCharactersInString(generalDelimitersToEncode + subDelimitersToEncode)
+            let allowedCharacterSet = (CharacterSet.urlQueryAllowed as NSCharacterSet).mutableCopy() as! NSMutableCharacterSet
+            allowedCharacterSet.removeCharacters(in: generalDelimitersToEncode + subDelimitersToEncode)
 
             var escaped = ""
 
             if #available(iOS 8.3, *) {
-                escaped = string.stringByAddingPercentEncodingWithAllowedCharacters(allowedCharacterSet) ?? string
+                escaped = string.addingPercentEncoding(withAllowedCharacters: allowedCharacterSet as CharacterSet) ?? string
 
             } else {
                 let batchSize = 50
@@ -130,12 +130,12 @@ class Networking {
                 
                 while index != string.endIndex {
                     let startIndex = index
-                    let endIndex = index.advancedBy(batchSize, limit: string.endIndex)
+                    let endIndex = <#T##Collection corresponding to `index`##Collection#>.index(index, offsetBy: batchSize, limitedBy: string.endIndex)
                     let range = startIndex..<endIndex
                     
-                    let substring = string.substringWithRange(range)
+                    let substring = string.substring(with: range)
                     
-                    escaped += substring.stringByAddingPercentEncodingWithAllowedCharacters(allowedCharacterSet) ?? substring
+                    escaped += substring.addingPercentEncoding(withAllowedCharacters: allowedCharacterSet) ?? substring
                     
                     index = endIndex
                 }
@@ -145,14 +145,14 @@ class Networking {
         }
     }
 
-    func request(URLString: String, method: Method, parameters: [String: AnyObject]? = nil, encoding: ParameterEncoding = .URL, headers: [String: String]? = nil, completionHandler: (NSDictionary?, NSURLResponse?, NSError?) -> Void) {
+    func request(_ URLString: String, method: Method, parameters: [String: AnyObject]? = nil, encoding: ParameterEncoding = .url, headers: [String: String]? = nil, completionHandler: @escaping (NSDictionary?, URLResponse?, NSError?) -> Void) {
 
-        guard let URL = NSURL(string: URLString) else {
+        guard let URL = URL(string: URLString) else {
             return
         }
 
-        let mutableURLRequest = NSMutableURLRequest(URL: URL)
-        mutableURLRequest.HTTPMethod = method.rawValue
+        let mutableURLRequest = NSMutableURLRequest(url: URL)
+        mutableURLRequest.httpMethod = method.rawValue
 
         if let headers = headers {
             for (headerField, headerValue) in headers {
@@ -162,65 +162,65 @@ class Networking {
 
         let request = encoding.encode(mutableURLRequest, parameters: parameters)
 
-        let task = session.dataTaskWithRequest(request) { (data, response, error) -> Void in
+        let task = session.dataTask(with: request, completionHandler: { (data, response, error) -> Void in
 
             var JSON: NSDictionary?
 
             defer {
-                dispatch_async(dispatch_get_main_queue()) {
-                    completionHandler(JSON, response, error)
+                DispatchQueue.main.async {
+                    completionHandler(JSON, response, error as NSError?)
                 }
             }
 
             guard let validData = data,
-                let JSONData = try? NSJSONSerialization.JSONObjectWithData(validData, options: .AllowFragments) as? NSDictionary else {
+                let JSONData = try? JSONSerialization.jsonObject(with: validData, options: .allowFragments) as? NSDictionary else {
                     print("JSON could not be serialized because input data was nil.")
                     return
             }
 
             JSON = JSONData
-        }
+        }) 
 
         task.resume()
     }
 
-    func upload(URLString: String, parameters: [String: AnyObject], completionHandler: (NSDictionary?, NSURLResponse?, NSError?) -> Void) {
+    func upload(_ URLString: String, parameters: [String: AnyObject], completionHandler: @escaping (NSDictionary?, URLResponse?, NSError?) -> Void) {
 
         let tuple = urlRequestWithComponents(URLString, parameters: parameters)
 
-        guard let request = tuple.request, data = tuple.data else {
+        guard let request = tuple.request, let data = tuple.data else {
             return
         }
 
-        let uploadTask = session.uploadTaskWithRequest(request, fromData: data) { (data, response, error) -> Void in
+        let uploadTask = session.uploadTask(with: request, from: data, completionHandler: { (data, response, error) -> Void in
             var JSON: NSDictionary?
 
             defer {
-                dispatch_async(dispatch_get_main_queue()) {
-                    completionHandler(JSON, response, error)
+                DispatchQueue.main.async {
+                    completionHandler(JSON, response, error as NSError?)
                 }
             }
 
             guard let validData = data,
-                let JSONData = try? NSJSONSerialization.JSONObjectWithData(validData, options: .AllowFragments) as? NSDictionary else {
+                let JSONData = try? JSONSerialization.jsonObject(with: validData, options: .allowFragments) as? NSDictionary else {
                     print("JSON could not be serialized because input data was nil.")
                     return
             }
 
             JSON = JSONData
-        }
+        }) 
 
         uploadTask.resume()
     }
 
-    func urlRequestWithComponents(URLString: String, parameters: [String: AnyObject], encoding: ParameterEncoding = .URL) -> (request: NSURLRequest?, data: NSData?) {
+    func urlRequestWithComponents(_ URLString: String, parameters: [String: AnyObject], encoding: ParameterEncoding = .url) -> (request: URLRequest?, data: Data?) {
 
-        guard let URL = NSURL(string: URLString) else {
+        guard let URL = URL(string: URLString) else {
             return (nil, nil)
         }
 
-        let mutableURLRequest = NSMutableURLRequest(URL: URL)
-        mutableURLRequest.HTTPMethod = Method.POST.rawValue
+        let mutableURLRequest = NSMutableURLRequest(url: URL)
+        mutableURLRequest.httpMethod = Method.POST.rawValue
         let boundaryConstant = "NET-POST-boundary-\(arc4random())-\(arc4random())"
         let contentType = "multipart/form-data;boundary="+boundaryConstant
         mutableURLRequest.setValue(contentType, forHTTPHeaderField: "Content-Type")
@@ -230,39 +230,39 @@ class Networking {
         // add parameters
         for (key, value) in parameters {
 
-            guard let encodeBoundaryData = "\r\n--\(boundaryConstant)\r\n".dataUsingEncoding(NSUTF8StringEncoding) else {
+            guard let encodeBoundaryData = "\r\n--\(boundaryConstant)\r\n".data(using: String.Encoding.utf8) else {
                 return (nil, nil)
             }
 
-            uploadData.appendData(encodeBoundaryData)
+            uploadData.append(encodeBoundaryData)
 
-            if let imageData = value as? NSData {
+            if let imageData = value as? Data {
 
                 let filename = arc4random()
                 let filenameClause = "filename=\"\(filename)\""
                 let contentDispositionString = "Content-Disposition: form-data; name=\"\(key)\";\(filenameClause)\r\n"
-                let contentDispositionData = contentDispositionString.dataUsingEncoding(NSUTF8StringEncoding)
-                uploadData.appendData(contentDispositionData!)
+                let contentDispositionData = contentDispositionString.data(using: String.Encoding.utf8)
+                uploadData.append(contentDispositionData!)
 
                 // append content type
                 let contentTypeString = "Content-Type: image/JPEG\r\n\r\n"
-                guard let contentTypeData = contentTypeString.dataUsingEncoding(NSUTF8StringEncoding) else {
+                guard let contentTypeData = contentTypeString.data(using: String.Encoding.utf8) else {
                     return (nil, nil)
                 }
-                uploadData.appendData(contentTypeData)
-                uploadData.appendData(imageData)
+                uploadData.append(contentTypeData)
+                uploadData.append(imageData)
 
             } else{
 
-                guard let encodeDispositionData = "Content-Disposition: form-data; name=\"\(key)\"\r\n\r\n\(value)".dataUsingEncoding(NSUTF8StringEncoding) else {
+                guard let encodeDispositionData = "Content-Disposition: form-data; name=\"\(key)\"\r\n\r\n\(value)".data(using: String.Encoding.utf8) else {
                     return (nil, nil)
                 }
-                uploadData.appendData(encodeDispositionData)
+                uploadData.append(encodeDispositionData)
             }
         }
 
-        uploadData.appendData("\r\n--\(boundaryConstant)--\r\n".dataUsingEncoding(NSUTF8StringEncoding)!)
+        uploadData.append("\r\n--\(boundaryConstant)--\r\n".data(using: String.Encoding.utf8)!)
 
-        return (encoding.encode(mutableURLRequest, parameters: nil), uploadData)
+        return (encoding.encode(mutableURLRequest, parameters: nil), uploadData as Data)
     }
 }
