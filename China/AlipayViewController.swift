06b6966f1cbe9daa08ef711cc3dbc46f3be48c4d
Lanford <gavinflying@126.com>
2016-10-16 19:36:11 +0800

    添加分享图片和链接到支付宝生活圈

--- China/AlipayViewController.swift
+++ China/AlipayViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 import MonkeyKing
 
 class AlipayViewController: UIViewController {
+    
+    @IBOutlet fileprivate var segmentedControl: UISegmentedControl!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,9 +51,22 @@ class AlipayViewController: UIViewController {
     }
 
     fileprivate func shareInfo(_ info: MonkeyKing.Info) {
-        let message = MonkeyKing.Message.alipay(.friends(info: info))
-        MonkeyKing.deliver(message) { result in
-            print("result: \(result)")
+        
+        var message: MonkeyKing.Message?
+        
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            message = MonkeyKing.Message.alipay(.friends(info: info))
+        case 1:
+            message = MonkeyKing.Message.alipay(.timeline(info: info))
+        default:
+            break
+        }
+        
+        if let message = message {
+            MonkeyKing.deliver(message) { result in
+                print("result: \(result)")
+            }
         }
     }
 
