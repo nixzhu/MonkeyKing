753ca3ef87f7f095335b684d35e6e903dca2b9a0
Limon <fengninglong@gmail.com>
2017-02-28 13:37:42 +0800

    clean code

--- Sources/MonkeyKing.swift
+++ Sources/MonkeyKing.swift
@@ -1573,31 +1573,6 @@ extension MonkeyKing {
     }
 }
 
-
-class CloseButton: UIButton {
-    
-    override func draw(_ rect: CGRect) {
-
-        let circleWidth: CGFloat = 28.0
-        let circlePathX = (rect.width - circleWidth) / 2.0
-        let circlePathY = (rect.height - circleWidth) / 2.0
-        let circlePathRect = CGRect(x: circlePathX, y: circlePathY, width: circleWidth, height: circleWidth)
-        let circlePath = UIBezierPath(ovalIn: circlePathRect)
-        UIColor(white: 0.8, alpha: 1.0).setFill()
-        circlePath.fill()
-
-        let xPath = UIBezierPath()
-        xPath.lineWidth = 2.0
-        let offset: CGFloat = (bounds.width - circleWidth) / 2.0
-        xPath.move(to: CGPoint(x: offset + circleWidth / 4.0, y: offset + circleWidth / 4.0))
-        xPath.addLine(to: CGPoint(x: offset + 3.0 * circleWidth / 4.0, y: offset + 3.0 * circleWidth / 4.0))
-        xPath.move(to: CGPoint(x: offset + circleWidth / 4.0, y: offset + 3.0 * circleWidth / 4.0))
-        xPath.addLine(to: CGPoint(x: offset + 3.0 * circleWidth / 4.0, y: offset + circleWidth / 4.0))
-        UIColor.white.setStroke()
-        xPath.stroke()
-    }
-}
-
 // MARK: Private Extensions
 
 private extension Set {
@@ -1850,3 +1825,26 @@ private extension UIImage {
     }
 }
 
+class CloseButton: UIButton {
+
+    override func draw(_ rect: CGRect) {
+
+        let circleWidth: CGFloat = 28.0
+        let circlePathX = (rect.width - circleWidth) / 2.0
+        let circlePathY = (rect.height - circleWidth) / 2.0
+        let circlePathRect = CGRect(x: circlePathX, y: circlePathY, width: circleWidth, height: circleWidth)
+        let circlePath = UIBezierPath(ovalIn: circlePathRect)
+        UIColor(white: 0.8, alpha: 1.0).setFill()
+        circlePath.fill()
+
+        let xPath = UIBezierPath()
+        xPath.lineWidth = 2.0
+        let offset: CGFloat = (bounds.width - circleWidth) / 2.0
+        xPath.move(to: CGPoint(x: offset + circleWidth / 4.0, y: offset + circleWidth / 4.0))
+        xPath.addLine(to: CGPoint(x: offset + 3.0 * circleWidth / 4.0, y: offset + 3.0 * circleWidth / 4.0))
+        xPath.move(to: CGPoint(x: offset + circleWidth / 4.0, y: offset + 3.0 * circleWidth / 4.0))
+        xPath.addLine(to: CGPoint(x: offset + 3.0 * circleWidth / 4.0, y: offset + circleWidth / 4.0))
+        UIColor.white.setStroke()
+        xPath.stroke()
+    }
+}
