a87122404eb8a16e2ca208765ddbac873c38f7ca
nixzhu <zhuhongxu@gmail.com>
2018-04-17 15:00:27 +0800

    new open url delegate method

--- China/AppDelegate.swift
+++ China/AppDelegate.swift
@@ -7,7 +7,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
         if MonkeyKing.handleOpenURL(url) {
             return true
         }
