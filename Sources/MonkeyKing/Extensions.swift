358bb91abf418b2d36ad6e8212c5751da2e5d59d
nixzhu <zhuhongxu@gmail.com>
2020-09-14 18:58:18 +0800

    code style

--- Sources/MonkeyKing/Extensions.swift
+++ Sources/MonkeyKing/Extensions.swift
@@ -1,5 +1,6 @@
-import MobileCoreServices
+
 import UIKit
+import MobileCoreServices
 import CommonCrypto
 
 extension Set {
--- Sources/MonkeyKing/MonkeyKing+handleOpenURL.swift
+++ Sources/MonkeyKing/MonkeyKing+handleOpenURL.swift
@@ -1,5 +1,5 @@
+
 import UIKit
-import Foundation
 import Security
 
 extension MonkeyKing {
